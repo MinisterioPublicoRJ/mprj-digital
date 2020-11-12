ffbce90e46a8d07a69157fd7c3b673d54656a290
Layla <laylacsa@msn.com>
2020-11-12 12:48:30 -0300

    Limpando coisas padrão

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bem vindo ao MPRJ Digital com Lasers!
+      </header>
+    </div>
+  );
+}
+
+export default App;
