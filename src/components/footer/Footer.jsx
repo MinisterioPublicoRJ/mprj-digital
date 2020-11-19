9c49f150fa57c01f95cdc95b4945efc3e7735009
Layla <laylacsa@msn.com>
2020-11-19 19:16:20 -0300

    Footer base, missing css refinings and menu

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="footer-outer">
+      <div className="footer-map" />
+      <div className="footer-floating">
+        <div className="footer-form-header">
+          <h1>Dúvidas e Reclamações?</h1>
+          <p>Espaço aberto para ouvir suas dúvidas e comentários a respeito da plataforma, dos produtos, ou de algum dado que está a disposição.</p>
+        </div>
+        <form className="footer-form-body">
+          <input></input>
+          <input></input>
+          <input></input>
+          <button>Enviar</button>
+        </form>
+      </div>
+      <div className="footer-menu">a</div>
+    </div>
+  );
 }
