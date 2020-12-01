6cba1c771ecb0fc0dfdc481eceda7e29640bf1a5
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2020-12-01 17:54:38 -0300

    Feature #12 Open Data Component Page Fix files convention

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import '../themes/themes.css';
 import Router from '../router/Router';
 import Header from '../components/header/header';
+import OpenData from '../components/openData/OpenData';
 import Footer from '../components/footer/footer';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Router />
+    <OpenData />
     <Footer />
   </BrowserRouter>
 );
