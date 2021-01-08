a93c86406caf8b1423615f1edfd23210058e74fa
re-andrade1987 <renataandrade1987@hotmail.com>
2021-01-08 13:04:38 -0300

    Ajusting navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,8 +9,9 @@ export default function Navbar() {
     <navbar className="navbar">
       <Logo alt="logo" />
       <section className="navbar-links">
-        <NavLink to="/" end>
+        <NavLink to="/" end className="dropdow-button">
           O que é o MPRJ Digital?
+          <span>Encontre aqui todas as nossas Acervo de Dados</span>
         </NavLink>
         <DropdownButton className="dropdow-button" id="dropdown-basic-button" title="Parceiros">
           <Dropdown.Item>
@@ -35,7 +36,7 @@ export default function Navbar() {
           </Dropdown.Item>
         </DropdownButton>
         <DropdownButton className="dropdow-button" id="dropdown-basic-button" title="Repositórios">
-          <span>Encontre aqui todas as nossas Acervo de Dados</span>
+          <span>Encontre aqui todas os nossos acervos de Dados</span>
           <Dropdown.Item>
             <NavLink to="/produto">Integra</NavLink>
           </Dropdown.Item>
@@ -46,7 +47,10 @@ export default function Navbar() {
             <NavLink to="/produto">Ouvidoria</NavLink>
           </Dropdown.Item>
         </DropdownButton>
-        <NavLink to="home">Fale Conosco</NavLink>
+        <NavLink to="home" className="dropdow-button" id="dropdown-basic-button">
+          Fale Conosco
+          <span>Adoramos receber elogios, mas o seu feedback e crítica são ainda melhores</span>
+        </NavLink>
         <NavLink to="home">Dúvidas e Reclamações</NavLink>
       </section>
     </navbar>
