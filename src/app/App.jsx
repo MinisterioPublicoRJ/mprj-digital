003249ea6d2f89c54cfaa5229f97ba30f4fe77af
Layla <laylacsa@msn.com>
2021-01-11 10:45:56 -0300

    Adding default styles for p and h1

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import './App.css';
 import '../themes/themes.css';
+
 import Router from '../router/Router';
 import Footer from '../components/footer/footer';
 import Navbar from '../components/navbar/navbar';
