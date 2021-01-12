40211e526ea196fdcd590b3ff30b5bb7801443e1
re-andrade1987 <renataandrade1987@hotmail.com>
2021-01-11 21:06:41 -0300

    Ajustando animações da navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
-import { DropdownButton, Dropdown } from 'react-bootstrap';
 import Logo from '../../assets/logo';
 
 export default function Navbar() {
@@ -9,54 +8,67 @@ export default function Navbar() {
     <navbar className="navbar">
       <Logo alt="logo" />
       <section className="navbar-links">
-        <NavLink to="/" end className="dropdow-button">
-          O que é o MPRJ Digital?
-          <span>Encontre aqui todas as nossas Acervo de Dados</span>
-        </NavLink>
-        <DropdownButton className="dropdow-button" id="dropdown-basic-button" title="Parceiros">
-          <span>Conheça os setores do MPRJ envolvidos nessa iniciativa</span>
-          <Dropdown.Item>
-            <NavLink to="/produto">Gate</NavLink>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <NavLink to="/produto">Cenpe</NavLink>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <NavLink to="/produto">Csi</NavLink>
-          </Dropdown.Item>
-        </DropdownButton>
-        <DropdownButton className="dropdow-button" id="dropdown-basic-button" title="Produtos">
-          <span>Conheça os setores do MPRJ envolvidos nessa iniciativa</span>
-          <Dropdown.Item>
-            <NavLink to="/produto">Integra</NavLink>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <NavLink to="/produto">Farol</NavLink>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <NavLink to="/produto">Ouvidoria</NavLink>
-          </Dropdown.Item>
-        </DropdownButton>
-        <DropdownButton className="dropdow-button" id="dropdown-basic-button" title="Repositórios">
-          <span>Encontre aqui todas os nossos acervos de Dados</span>
-          <Dropdown.Item>
-            <NavLink to="/produto">Integra</NavLink>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <NavLink to="/produto">Farol</NavLink>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <NavLink to="/produto">Ouvidoria</NavLink>
-          </Dropdown.Item>
-        </DropdownButton>
-        <NavLink to="home" className="dropdow-button">
-          Fale Conosco
-          <span>Adoramos receber elogios, mas o seu feedback e crítica são ainda melhores</span>
-        </NavLink>
-        <NavLink className="dropdow-button" to="home">
-          Dúvidas e Reclamações
-          <span>Não hesite em reclamar ou pedir ajuda caso tenha algum problema.</span>
-        </NavLink>
+        <div className="tooltip">
+          <NavLink to="/" end className="dropdow-button">
+            O que é o MPRJ Digital?
+          </NavLink>
+          <span className="tooltiptext">Conheça a nossa visão de dados abertos</span>
+        </div>
+        <div className="dropdown">
+          <div className="tooltip">
+            <button type="button" className="dropbtn" id="basic-button">
+              Parceiros
+            </button>
+            <span className="tooltiptext">Navegue e conheça nossos produtos e sistemas</span>
+            <div className="dropdown-content">
+              <NavLink to="/produto">Gate</NavLink>
+              <NavLink to="/produto">Cenpe</NavLink>
+              <NavLink to="/produto">Csi</NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="dropdown">
+          <div className="tooltip">
+            <button type="button" className="dropbtn" id="basic-button">
+              Produtos
+            </button>
+            <span className="tooltiptext">Navegue e conheça nossos produtos e sistemas</span>
+            <div className="dropdown-content">
+              <NavLink to="/produto">Integra</NavLink>
+              <NavLink to="/produto">Farol</NavLink>
+              <NavLink to="/produto">Ouvidoria</NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="dropdown">
+          <div className="tooltip">
+            <button type="button" className="dropbtn" id="basic-button">
+              Repositórios
+            </button>
+            <span className="tooltiptext">Encontre aqui todas os nossos acervos de dados</span>
+            <div className="dropdown-content">
+              <NavLink to="/produto">Integra</NavLink>
+              <NavLink to="/produto">Farol</NavLink>
+              <NavLink to="/produto">Ouvidoria</NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="tooltip">
+          <NavLink to="home" className="dropdow-button">
+            Fale Conosco
+            <span className="tooltiptext">
+              Adoramos receber elogios, mas o seu feedback e crítica são ainda melhores
+            </span>
+          </NavLink>
+        </div>
+        <div className="tooltip">
+          <NavLink className="dropdow-button" to="home">
+            Dúvidas e Reclamações
+            <span className="tooltiptext">
+              Não hesite em reclamar ou pedir ajuda caso tenha algum problema.
+            </span>
+          </NavLink>
+        </div>
       </section>
     </navbar>
   );
