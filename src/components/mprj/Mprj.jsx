9e6250a7cdbb7d393a3cc47c89da722496755590
Layla <laylacsa@msn.com>
2021-01-13 12:54:25 -0300

    Everything but the images

--- src/components/mprj/Mprj.jsx
+++ src/components/mprj/Mprj.jsx
@@ -13,12 +13,41 @@ export default function Mprj() {
         conhecimento.
       </p>
       <div className="mprjDigital-moreInfo">
-        <div>
-          ouhiuh
-          <span> aopjfsdh </span>
+        <div className="mprjDigital-itemOuter">
+          <h6> Por quê? </h6>
+          <p>
+            Porque ainda que já existam iniciativas de transparência ativa (portal da transparência)
+            e passiva (requisições via Lei de Acesso à Informação), há espaço para um terceiro
+            espaço, que promova não só a transparência a nível de disponibilizar informações, mas
+            também de auxiliar a entendê-las, especialmente com relação à filosofia de construir o
+            Ministério Público do futuro, pautado por atuação científica e estratégica, auxiliada
+            pelos recursos tecnológicos desenvolvidos nos últimos anos.
+          </p>
         </div>
-        <div>2</div>
-        <div>3</div>
+        <div className="mprjDigital-itemOuter">
+          <h6> Como? </h6>
+          <p>
+            Por meio da disponibilização de acesso a produtos que servem de subsídio à atuação
+            ministerial em um espaço centralizado, facilitando o acesso. Além disso, são fornecidas
+            explicações sobre sua construção, funcionamento e funções, de modo a tornar o menu de
+            ferramentas acessível a todos e todas. Por último, mas não menos importantes, são
+            fornecidas as bases de dados (que não têm algum nível de proteção legal ou sigilo) e
+            componentes de código que deram forma aos recursos tecnológicos, permitindo que sejam
+            auditados, replicados, questionados e melhorados por agentes externos, criando ambiente
+            de transparência e cooperação com quem desejar o mesmo.
+          </p>
+        </div>
+        <div className="mprjDigital-itemOuter">
+          <h6> Baseado em quê? </h6>
+          <p>
+            Nos valores de governos abertos: transparência, integridade, participação popular e
+            tecnologias e inovação, além de avançar com a ideia de transparência ao nível de
+            transparência efetiva, capaz de não só mostrar, mas também auxiliar a entender o que os
+            dados e informações querem dizer. A plataforma se baseia totalmente no valor dos dados,
+            essenciais para se falar em governos abertos, além de foco no usuário para promover a
+            necessária efetividade.
+          </p>
+        </div>
       </div>
       {/*
 
