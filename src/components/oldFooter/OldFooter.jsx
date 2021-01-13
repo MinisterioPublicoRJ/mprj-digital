ea8b84d9e39e732a64e53a079df164770ef36f96
Layla <laylacsa@msn.com>
2021-01-13 17:36:40 -0300

    Footer alterations

--- src/components/oldFooter/OldFooter.jsx
+++ src/components/oldFooter/OldFooter.jsx
@@ -1,39 +1,70 @@
 import React from 'react';
-import './Footer.css';
+import './OldFooter.css';
 import Logo from '../../assets/logo';
 
-export default function Footer() {
+export default function oldFooter() {
+  function handleFormSubmit() {
+    alert('Mensagem enviada com sucesso!');
+  }
+
   return (
-    <div className="footer-outer">
-      <div className="footer-menu">
-        <div className="footer-menu-column">
+    <div className="oldFooter-outer">
+      <div className="oldFooter-map" />
+      <div className="oldFooter-floating">
+        <div className="oldFooter-form-header">
+          <h1>Dúvidas e Reclamações?</h1>
+          <p>
+            Espaço aberto para ouvir suas dúvidas e comentários a respeito da plataforma, dos
+            produtos, ou de algum dado que está a disposição.
+          </p>
+        </div>
+        <form className="oldFooter-form-body">
+          <div>
+            <input type="text" placeholder="Nome" />
+            <input type="text" placeholder="Email" />
+          </div>
+          <div>
+            <textarea placeholder="Mensagem" />
+          </div>
+          <div>
+            <button type="button" onClick={() => handleFormSubmit()}>
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="oldFooter-menu">
+        <div className="oldFooter-menu-column">
           <Logo alt="Logo MPRJ Digital" />
           <span className="create-space">© Ministério Público do Estado do Rio de Janeiro</span>
-          <div className="footer-menu-icons">
+          <div className="oldFooter-menu-icons">
             <a
               href="https://twitter.com/MP_RJ"
               target="_blank"
               rel="noreferrer"
-              className="footer-menu-icon-twitter"
-              aria-label="Twitter"
-            />
+              className="oldFooter-menu-icon-twitter"
+            >
+              Twitter
+            </a>
             <a
               href="https://www.instagram.com/mprj.oficial/"
               target="_blank"
               rel="noreferrer"
-              className="footer-menu-icon-instagram"
-              aria-label="Instagram"
-            />
+              className="oldFooter-menu-icon-instagram"
+            >
+              Instagram
+            </a>
             <a
               href="https://www.facebook.com/MPRJ.Oficial/"
               target="_blank"
               rel="noreferrer"
-              className="footer-menu-icon-facebook"
-              aria-label="Facebook"
-            />
+              className="oldFooter-menu-icon-facebook"
+            >
+              Facebook
+            </a>
           </div>
         </div>
-        <div className="footer-menu-column">
+        <div className="oldFooter-menu-column">
           <h3>PRODUTOS EM DESTAQUE</h3>
           <a href="./#">Parquet Digital</a>
           <a href="./#">Farol</a>
@@ -42,13 +73,13 @@ export default function Footer() {
           <a href="/#">Painel do Saneamento</a>
           <a href="/#">Painel de Compras</a>
         </div>
-        <div className="footer-menu-column">
+        <div className="oldFooter-menu-column">
           <h3>MPRJ DIGITAL</h3>
           <a href="/#">Quem Somos?</a>
           <a href="/#">Parceiros</a>
           <a href="/#">Contato</a>
         </div>
-        <div className="footer-menu-column">
+        <div className="oldFooter-menu-column">
           <h3>OUVIDORIA</h3>
           <a
             href="http://www.mprj.mp.br/comunicacao/ouvidoria"
