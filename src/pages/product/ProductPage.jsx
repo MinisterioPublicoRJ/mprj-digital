697c094f0574233a7df9e0eea22702d231a1f02d
Layla <laylacsa@msn.com>
2021-01-22 16:31:49 -0300

    Dynamic header background

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import './ProductPage.css';
+import PRODUCTS_CONST from './productsConsts';
 
 export default function Produto() {
+  const { key } = useParams();
+  const { background } = PRODUCTS_CONST[key];
+
   return (
     <article className="productPage-outer">
-      <div className="productPage-map" />
+      <div className="productPage-img" style={{ backgroundImage: `url(${background})` }} />
       <div className="productPage-presentation">here goes explanation!</div>
       <div className="productPage-body">here goes all the data!</div>
     </article>
---
+++ src/pages/product/productsConsts.js
@@ -0,0 +1,13 @@
+import bgFarol from '../../assets/images/bgFarol.png';
+import bgParquet from '../../assets/images/bgParquet.png';
+
+const PRODUCTS_CONST = {
+  farol: {
+    background: bgFarol,
+  },
+  parquet_digital: {
+    background: bgParquet,
+  },
+};
+
+export default PRODUCTS_CONST;
