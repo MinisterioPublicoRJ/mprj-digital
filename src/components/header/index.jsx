5ceee1edbd4ace95d0f7db52b6978a173d2a6a98
re-andrade1987 <renataandrade1987@hotmail.com>
2021-01-25 10:56:28 -0300

    Organizing files

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,16 @@
 export { default as ButtonHeader } from './buttonHeaderItem/ButtonHeader';
 export { default as NavHeader } from './navHeaderItem/NavHeader';
 export { default as BgHeader } from './bgHeaderItem/BgHeader';
+export { default as farolBg } from '../../assets/ImgsHeader/bg-farol.png';
+export { default as mpBg } from '../../assets/ImgsHeader/bg-mp.png';
+export { default as parquet } from '../../assets/ImgsHeader/bg-parquet.png';
+export { default as ARROW } from '../../assets/ImgsHeader/arrow.png';
+export { default as REPOSITORIO } from '../../assets/ImgsHeader/grupo.png';
+export { default as SEARCH } from '../../assets/ImgsHeader/search.png';
+export { default as CLOCK } from '../../assets/ImgsHeader/clock.png';
+export { default as FAROL } from '../../assets/ImgsHeader/farol.png';
+export { default as PLANILHA } from '../../assets/ImgsHeader/planilhas.png';
+export { default as DOCS } from '../../assets/ImgsHeader/docs.png';
+export { default as ROBOT } from '../../assets/ImgsHeader/robot.png';
+
 
--- src/components/header/mockProdutosHeader.js
+++ src/components/header/mockProdutosHeader.js
@@ -1,14 +1,16 @@
-import farolBg from '../../assets/ImgsHeader/bg-farol.png';
-import mpBg from '../../assets/ImgsHeader/bg-mp.png';
-import parquet from '../../assets/ImgsHeader/bg-parquet.png';
-import ARROW from '../../assets/ImgsHeader/arrow.png';
-import REPOSITORIO from '../../assets/ImgsHeader/grupo.png';
-import SEARCH from '../../assets/ImgsHeader/search.png';
-import CLOCK from '../../assets/ImgsHeader/clock.png';
-import FAROL from '../../assets/ImgsHeader/farol.png';
-import PLANILHA from '../../assets/ImgsHeader/planilhas.png';
-import DOCS from '../../assets/ImgsHeader/docs.png';
-import ROBOT from '../../assets/ImgsHeader/robot.png';
+import {
+  farolBg,
+  mpBg,
+  parquet,
+  FAROL,
+  PLANILHA,
+  ARROW,
+  REPOSITORIO,
+  SEARCH,
+  CLOCK,
+  DOCS,
+  ROBOT,
+} from './index';
 
 export const MOCKPRODUTOSHEADER = [
   {
