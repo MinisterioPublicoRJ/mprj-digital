e5b41bbccfe98faf2e4a52bfb58d92825448d62c
Layla <laylacsa@msn.com>
2021-01-26 13:46:37 -0300

    Fixing linter problems

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,3 @@
 export { default as ButtonHeader } from './buttonHeaderItem/ButtonHeader';
 export { default as NavHeader } from './navHeaderItem/NavHeader';
 export { default as BgHeader } from './bgHeaderItem/BgHeader';
-
