a2159fea74a87782314a40520b8c51f9bfcb0a1c
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2021-01-29 11:27:26 -0300

    WIP Feature/Parners

--- src/pages/partner/PartnerPage.jsx
+++ src/pages/partner/PartnerPage.jsx
@@ -7,48 +7,82 @@ import { Partners } from '../../components';
 import { PARTNERS_CONST } from './partnersData';
 
 export default function PartnerPage() {
-  const { key } = useParams();
-  const partnerFiltered = PARTNERS_CONST.filter((partner) => partner.id === key);
+  const { partnerId, subpageId } = useParams();
+  const partnerFiltered = PARTNERS_CONST.filter((partner) => partner.id === partnerId);
+  const subpageData = (partnerFiltered[0].subpages || []).filter(
+    (subpages) => subpages.id === subpageId,
+  );
+  const featuredTopics = (subpageData[0].topics || []).filter((topics) => topics.featured === true);
+  const defaultTopics = (subpageData[0].topics || []).filter((topics) => topics.featured === false);
+
+  function openForm() {
+    console.log('Opened Form');
+  }
+
+  const createLink = ({ url, action }) => {
+    if (url) {
+      window.open(url);
+      return;
+    }
+    action();
+  };
 
   return (
     <>
       <section className="partner-page-section">
         <div className="partner-page-header">
-          <div className="partner-page-header-title">
+          <div className="partner-page-title">
             <h1>{partnerFiltered[0].name}</h1>
-            <small>{partnerFiltered[0].desc}</small>
+            <p>{partnerFiltered[0].desc}</p>
           </div>
-          <navbar>
-            <div>
-              <a href="/#">Quem somos</a>
-            </div>
-            <div>
-              <a href="/#">Soluções</a>
-            </div>
-            <div>
-              <a href="/#">Podemos te ajudar?</a>
-            </div>
-            <div>
-              <a href="/#">Nosso time</a>
-            </div>
+          <navbar className="partner-page-navigation">
+            {partnerFiltered[0].subpages.map((subpage) => (
+              <div key={subpage.id}>
+                <a className="partner-page-navigation-link" href={subpage.id}>
+                  {subpage.call}
+                </a>
+              </div>
+            ))}
           </navbar>
         </div>
         <div className="partner-page-content">
           <div className="partner-page-left">
-            <div className="partner-page-logo">
-              <img
-                style={{ backgroundImage: `url(${partnerFiltered[0].img})` }}
-                alt={partnerFiltered[0].name}
-              />
-            </div>
-            <div className="partner-page-about">
-              <h3>Quem somos?</h3>
-              <p className="text-justify">{partnerFiltered[0].about}</p>
+            <div
+              className="partner-page-logo"
+              style={{ backgroundImage: `url(${partnerFiltered[0].img})` }}
+            />
+            <div className="partner-page-featured">
+              {featuredTopics.map((featured) => (
+                <div key={featured.id}>
+                  <h3>{featured.title}</h3>
+                  <p className="">{featured.smalltext}</p>
+                </div>
+              ))}
             </div>
           </div>
           <div className="partner-page-right">
-            <h3>Nossos pilares</h3>
-            <p>{partnerFiltered[0].fundamentals}</p>
+            <div className="partner-page-topics">
+              {defaultTopics.map((topic, i) => (
+                <div key={topic.id}>
+                  <h3>{topic.title}</h3>
+                  <p>{topic.smalltext}</p>
+                </div>
+              ))}
+            </div>
+            <div className="partner-page-cards">
+              {(subpageData[0].cards || []).map((card) => (
+                <div key={card.id} className={`partner-page-card ${card.type}`}>
+                  {card.img ? <img src={card.img} alt={card.alt} /> : null}
+                  <h4>{card.title}</h4>
+                  {card.smalltext ? <p>{card.smalltext}</p> : null}
+                  {card.link ? (
+                    <div>
+                      <a href={card.link}>{card.call}</a>
+                    </div>
+                  ) : null}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
--- src/pages/partner/partnersData.js
+++ src/pages/partner/partnersData.js
@@ -13,13 +13,20 @@ export const PARTNERS_CONST = [
   {
     id: 'cadg',
     name: 'CADG',
+    img: CADG,
     desc: 'Coordenadoria de Análise, Diagnósticos e Geografia',
     subpages: [
       {
         id: 'sobre',
         call: 'Sobre nós',
-        title: 'Quem somos?',
         topics: [
+          {
+            id: 'quemSomos',
+            title: 'Quem somos?',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            featured: true,
+          },
           {
             id: 'pilares',
             title: 'Nossos pilares',
@@ -27,6 +34,7 @@ export const PARTNERS_CONST = [
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             link: '',
             call: '',
+            featured: false,
           },
         ],
         cards: [
@@ -39,7 +47,198 @@ export const PARTNERS_CONST = [
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
             link: '',
             call: '',
-            image: 'people-gear',
+            img: CADG,
+            alt: 'Img alt',
+          },
+          {
+            id: 'equipe2',
+            type: 'first',
+            background: 'transparent',
+            title: 'Equipe Multidisciplinar',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            link: '',
+            call: '',
+            img: CADG,
+            alt: 'Img alt',
+          },
+          {
+            id: 'equipe3',
+            type: 'first',
+            background: 'transparent',
+            title: 'Equipe Multidisciplinar',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            link: '',
+            call: '',
+            img: CADG,
+            alt: 'Img alt',
+          },
+        ],
+      },
+      {
+        id: 'solucoes',
+        call: 'Soluções',
+        topics: [
+          {
+            id: 'comoAjudar',
+            title: 'Como elas podem te ajudar?',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            featured: true,
+          },
+          {
+            id: 'introSolucoes',
+            title: 'Soluções',
+            smalltext:
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.',
+            link: '',
+            call: '',
+            featured: false,
+          },
+        ],
+        cards: [
+          {
+            id: 'parquetDigital',
+            type: 'second',
+            background: 'white',
+            title: 'Parquet Digital',
+            smalltext:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            link: 'https://parquetdigital.mprj.mp.br/',
+            call: 'Conheça a solução >',
+            img: '/Parquet_Digital.png',
+            alt: 'Img alt',
+          },
+          {
+            id: 'parquetDigital2',
+            type: 'second',
+            background: 'white',
+            title: 'Parquet Digital',
+            smalltext:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            link: 'https://parquetdigital.mprj.mp.br/',
+            call: 'Conheça a solução >',
+            img: '/Parquet_Digital.png',
+            alt: 'Img alt',
+          },
+          {
+            id: 'parquetDigital3',
+            type: 'second',
+            background: 'white',
+            title: 'Parquet Digital',
+            smalltext:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            link: 'https://parquetdigital.mprj.mp.br/',
+            call: 'Conheça a solução >',
+            img: '/Parquet_Digital.png',
+            alt: 'Img alt',
+          },
+          {
+            id: 'parquetDigital4',
+            type: 'second',
+            background: 'white',
+            title: 'Parquet Digital',
+            smalltext:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            link: 'https://parquetdigital.mprj.mp.br/',
+            call: 'Conheça a solução >',
+            img: '/Parquet_Digital.png',
+            alt: 'Img alt',
+          },
+          {
+            id: 'parquetDigital5',
+            type: 'second',
+            background: 'white',
+            title: 'Parquet Digital',
+            smalltext:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            link: 'https://parquetdigital.mprj.mp.br/',
+            call: 'Conheça a solução >',
+            img: '/Parquet_Digital.png',
+            alt: 'Img alt',
+          },
+          {
+            id: 'parquetDigital6',
+            type: 'second',
+            background: 'white',
+            title: 'Parquet Digital',
+            smalltext:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            link: 'https://parquetdigital.mprj.mp.br/',
+            call: 'Conheça a solução >',
+            img: '/Parquet_Digital.png',
+            alt: 'Img alt',
+          },
+        ],
+      },
+      {
+        id: 'podemos-ajudar',
+        call: 'Podemos te ajudar?',
+        topics: [
+          {
+            id: 'oQueFazemos',
+            title: 'O que fazemos para melhorar nossos serviços?',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            featured: true,
+          },
+          {
+            id: 'nosDiga',
+            title: 'Nos diga, qual sua...?',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            link: '',
+            call: '',
+            featured: false,
+          },
+        ],
+        cards: [
+          {
+            id: 'sugestao',
+            type: 'third',
+            background: 'white',
+            title: 'Sugestão',
+            smalltext: 'No que podemos melhorar pra te ajudar?',
+            link: 'openForm()',
+            call: '',
+            img: CADG,
+            alt: 'Img alt',
+          },
+          {
+            id: 'reclamacao',
+            type: 'third',
+            background: 'white',
+            title: 'Reclamação',
+            smalltext: 'O que está te deixando insatisfeito ?',
+            link: 'openForm()',
+            call: '',
+            img: CADG,
+            alt: 'Img alt',
+          },
+          {
+            id: 'elogios',
+            type: 'third',
+            background: 'white',
+            title: 'Elogios',
+            smalltext: 'No que podemos melhorar pra te ajudar?',
+            link: 'openForm()',
+            call: '',
+            img: CADG,
+            alt: 'Img alt',
+          },
+        ],
+      },
+      {
+        id: 'nosso-time',
+        call: 'Nosso time',
+        topics: [
+          {
+            id: 'nosso-time',
+            title: 'Nosso time',
+            smalltext:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            featured: true,
           },
         ],
       },
