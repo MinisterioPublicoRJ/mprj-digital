bd211674278af0aa894a3be84b1bb1a169358f23
re-andrade1987 <renataandrade1987@hotmail.com>
2021-04-26 15:57:23 -0300

    Ajustando desing na navegação

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,27 @@
+/* eslint-disable no-constant-condition */
 /* eslint-disable react/no-this-in-sfc */
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import './Navbar.css';
 import logo from '../../assets/logoNovo.png';
 
 export default function Navbar() {
+  const { pathname } = useLocation();
   return (
     <navbar className="navbar">
       <img className="logo" src={logo} alt="logo" />
       <section className="navbar-links">
         <div className="tooltip">
-          <a href="#mprjDigital" className="dropdow-button">
-            O que é o MPRJ Digital?
-          </a>
+          {pathname === '/' ? (
+            <a href="#mprjDigital" className="dropdow-button">
+              O que é o MPRJ Digital?
+            </a>
+          ) : (
+            <NavLink to="/" className="dropdow-button">
+              O que é o MPRJ Digital?
+            </NavLink>
+          )}
           <span className="tooltiptext">Conheça a nossa visão de dados abertos</span>
         </div>
         <div className="dropdown">
@@ -21,7 +29,7 @@ export default function Navbar() {
             <NavLink to="/" type="button" className="dropbtn-button" id="basic-button">
               Parceiros
             </NavLink>
-            <span className="tooltiptext">Navegue e conheça nossos produtos e sistemas</span>
+            <span className="tooltiptext">Conheça os setores envolvidos nessa iniciativa</span>
             <div className="dropdown-content">
               <NavLink to="/parceiro/cadg">Cadg</NavLink>
             </div>
@@ -44,12 +52,16 @@ export default function Navbar() {
           </div>
         </div>
         <div className="tooltip">
-          <a href="#repositorios" className="dropbtn">
-            Repositórios
-          </a>
-          {/* <NavLink to="/produto/" className="dropbtn" activeStyle={{ color: '#263859' }}>
-            Repositórios
-            </NavLink> */}
+          {pathname === '/' ? (
+            <a href="#repositorios" className="dropdow-button">
+              Repositórios
+            </a>
+          ) : (
+            <NavLink to="/" className="dropdow-button">
+              Repositórios
+            </NavLink>
+          )}
+          <span className="tooltiptext">Encontre aqui todas as nossas Acervo de Dados</span>
         </div>
         {/* <div className="dropdown">
           <div className="tooltip">
@@ -73,21 +85,17 @@ export default function Navbar() {
           </NavLink>
         </div> */}
         <div className="tooltip">
-          <a href="#rodape" className="dropbtn">
-            Dúvidas e Reclamações
-          </a>
-          {/* <NavLink to="/produto/" className="dropbtn" activeStyle={{ color: '#263859' }}>
-            Dúvidas e Reclamações
-          </NavLink> */}
+          {pathname === '/' ? (
+            <a href="#rodape" className="dropdow-button">
+              Dúvidas e Reclamações
+            </a>
+          ) : (
+            <NavLink to="/" className="dropdow-button">
+              Dúvidas e Reclamações
+            </NavLink>
+          )}
+          <span className="tooltiptext">Tem uma Reclamação ou precisa da nossa ajuda.</span>
         </div>
-        {/* <div className="tooltip">
-          <NavLink className="dropdow-button" to="home">
-            Dúvidas e Reclamações
-            <span className="tooltiptext">
-              Não hesite em reclamar ou pedir ajuda caso tenha algum problema.
-            </span>
-          </NavLink>
-        </div> */}
       </section>
     </navbar>
   );
