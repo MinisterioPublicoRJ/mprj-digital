fbd1b51bf7f8f1be4e89a0e0edd5491729b0d873
re-andrade1987 <renataandrade1987@hotmail.com>
2021-05-08 15:59:17 -0300

    Refatorando página de parceiros

--- src/pages/partner/PartnerPage.jsx
+++ src/pages/partner/PartnerPage.jsx
@@ -3,19 +3,12 @@ import { useParams, NavLink } from 'react-router-dom';
 import './PartnerPage.css';
 import { PartnersPageComponent } from '../../components';
 
-import { PARTNERS_CONST } from './partnersData';
+import PARTNERS_CONST2 from './partnersData2';
 
 export default function PartnerPage() {
   const { partnerId, subpageId } = useParams();
   const [formType, setFormType] = useState('');
-
-  const partnerFiltered = PARTNERS_CONST.filter((partner) => partner.id === partnerId);
-  const subpageIdToLoad = subpageId || partnerFiltered[0].subpages[0].id;
-  const subpageData = (partnerFiltered[0].subpages || []).filter(
-    (subpages) => subpages.id === subpageIdToLoad,
-  );
-  const featuredTopics = (subpageData[0].topics || []).filter((topics) => topics.featured === true);
-  const defaultTopics = (subpageData[0].topics || []).filter((topics) => topics.featured === false);
+  const { name, imgBg, desc, subpages, img } = PARTNERS_CONST2[partnerId];
 
   function openForm() {
     console.log('Opened Form');
@@ -32,16 +25,13 @@ export default function PartnerPage() {
   return (
     <>
       <section className="partner-page-section">
-        <div
-          className="partner-page-header"
-          style={{ backgroundImage: `url(${partnerFiltered[0].imgBg})` }}
-        >
+        <div className="partner-page-header" style={{ backgroundImage: `url(${imgBg})` }}>
           <div className="partner-page-title">
-            <h1>{partnerFiltered[0].name}</h1>
-            <p>{partnerFiltered[0].desc}</p>
+            <h1>{name}</h1>
+            <p>{desc}</p>
           </div>
           <navbar className="partner-page-navigation">
-            {partnerFiltered[0].subpages.map((subpage) => (
+            {subpages.map((subpage) => (
               <div key={subpage.id}>
                 <NavLink
                   className="partner-page-link"
@@ -56,30 +46,28 @@ export default function PartnerPage() {
         </div>
         <div className="partner-page-content">
           <div className="partner-page-left">
-            <div
-              className="partner-page-logo"
-              style={{ backgroundImage: `url(${partnerFiltered[0].img})` }}
-            />
+            <div className="partner-page-logo" style={{ backgroundImage: `url(${img})` }} />
             <div className="partner-page-featured">
-              {featuredTopics.map((featured) => (
-                <div key={featured.id}>
-                  <h3>{featured.title}</h3>
-                  <p dangerouslySetInnerHTML={{ __html: featured.smalltext }} />
+              {subpages[0].topics.map((topic) => (
+                <div key={topic.id}>
+                  <h3>{topic.title}</h3>
+                  <p>{topic.maintext}</p>
+                  <p>{topic.smalltext}</p>
                 </div>
               ))}
             </div>
           </div>
           <div className="partner-page-right">
             <div className="partner-page-topics">
-              {defaultTopics.map((topic) => (
-                <div key={topic.id}>
-                  <h3>{topic.title}</h3>
-                  <p>{topic.smalltext}</p>
+              {subpages[0].subtopics.map((subtopic) => (
+                <div key={subtopic.id}>
+                  <h3>{subtopic.title}</h3>
+                  <p>{subtopic.smalltext}</p>
                 </div>
               ))}
             </div>
             <div className="partner-page-cards">
-              {(subpageData[0].cards || []).map((card) => (
+              {(subpages[0].cards || []).map((card) => (
                 <div
                   key={card.id}
                   className={`partner-page-card ${card.type}`}
@@ -93,57 +81,6 @@ export default function PartnerPage() {
                 </div>
               ))}
             </div>
-            {subpageIdToLoad === 'podemos-ajudar' && formType !== '' ? (
-              <div className="partner-dynamic-content">
-                <h5>No que podemos melhorar</h5>
-                <div className="rounded-border-box">
-                  <navbar className="partner-page-navigation">
-                    <div>
-                      <NavLink to="#cidadao">Cidadão</NavLink>
-                    </div>
-                    <div>
-                      <NavLink to="#membro">Membro/Servidor</NavLink>
-                    </div>
-                  </navbar>
-                  <form className="partner-contact-form">
-                    <div className="inputs-box">
-                      <label htmlFor="nome">
-                        Nome
-                        <input type="text" id="nome" name="nome" />
-                      </label>
-                      <label htmlFor="email">
-                        E-mail
-                        <input type="text" id="email" name="email" />
-                      </label>
-                    </div>
-                    <div className="inputs-box">
-                      <label htmlFor="dataNascimento">
-                        Data de nascimento
-                        <input type="text" id="dataNascimento" name="dataNascimento" />
-                      </label>
-                      <label htmlFor="sexo">
-                        Sexo
-                        <input type="text" id="sexo" name="sexo" />
-                      </label>
-                      <label htmlFor="profissao">
-                        Profissão / Ocupação
-                        <input type="text" id="profissao" name="profissao" />
-                      </label>
-                    </div>
-                    <div className="inputs-box">
-                      <textarea placeholder="No que podemos melhorar?" />
-                    </div>
-                  </form>
-                </div>
-                <div className="partner-contact-form-action">
-                  <button type="button" className="partner-contact-button">
-                    enviar
-                    {' '}
-                    {formType}
-                  </button>
-                </div>
-              </div>
-            ) : null}
           </div>
         </div>
       </section>
