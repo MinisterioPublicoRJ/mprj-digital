58aa1227631ae040d7171ffd6632c1e16a6fa6f3
re-andrade1987 <renataandrade1987@hotmail.com>
2021-05-10 21:57:21 -0300

    Ajustando pr

--- src/pages/partner/PartnerPage.jsx
+++ src/pages/partner/PartnerPage.jsx
@@ -3,12 +3,18 @@ import { useParams, NavLink } from 'react-router-dom';
 import './PartnerPage.css';
 import { PartnersPageComponent } from '../../components';
 
-import PARTNERS_CONST2 from './partnersData2';
+import { PARTNERS_CONST } from './partnersData';
 
 export default function PartnerPage() {
   const { partnerId, subpageId } = useParams();
   const [formType, setFormType] = useState('');
-  const { name, imgBg, desc, subpages, img } = PARTNERS_CONST2[partnerId];
+
+  const partnerFiltered = PARTNERS_CONST.filter((partner) => partner.id === partnerId);
+  const subpageIdToLoad = subpageId || partnerFiltered[0].subpages[0].id;
+  const subpageData = (partnerFiltered[0].subpages || []).filter(
+    (subpages) => subpages.id === subpageIdToLoad,
+  );
+  const featuredTopics = subpageData[0].topics;
 
   function openForm() {
     console.log('Opened Form');
@@ -25,13 +31,16 @@ export default function PartnerPage() {
   return (
     <>
       <section className="partner-page-section">
-        <div className="partner-page-header" style={{ backgroundImage: `url(${imgBg})` }}>
+        <div
+          className="partner-page-header"
+          style={{ backgroundImage: `url(${partnerFiltered[0].imgBg})` }}
+        >
           <div className="partner-page-title">
-            <h1>{name}</h1>
-            <p>{desc}</p>
+            <h1>{partnerFiltered[0].name}</h1>
+            <p>{partnerFiltered[0].desc}</p>
           </div>
           <navbar className="partner-page-navigation">
-            {subpages.map((subpage) => (
+            {partnerFiltered[0].subpages.map((subpage) => (
               <div key={subpage.id}>
                 <NavLink
                   className="partner-page-link"
@@ -46,28 +55,31 @@ export default function PartnerPage() {
         </div>
         <div className="partner-page-content">
           <div className="partner-page-left">
-            <div className="partner-page-logo" style={{ backgroundImage: `url(${img})` }} />
+            <div
+              className="partner-page-logo"
+              style={{ backgroundImage: `url(${partnerFiltered[0].img})` }}
+            />
             <div className="partner-page-featured">
-              {subpages[0].topics.map((topic) => (
-                <div key={topic.id}>
-                  <h3>{topic.title}</h3>
-                  <p>{topic.maintext}</p>
-                  <p>{topic.smalltext}</p>
+              {featuredTopics.map((featured) => (
+                <div key={featured.id}>
+                  <h3>{featured.title}</h3>
+                  <p>{featured.smalltext}</p>
+                  <p>{featured.subsmalltext}</p>
                 </div>
               ))}
             </div>
           </div>
           <div className="partner-page-right">
             <div className="partner-page-topics">
-              {subpages[0].subtopics.map((subtopic) => (
-                <div key={subtopic.id}>
-                  <h3>{subtopic.title}</h3>
-                  <p>{subtopic.smalltext}</p>
+              {featuredTopics.map((featured) => (
+                <div key={featured.id}>
+                  <h3>{featured.title2}</h3>
+                  <p>{featured.smalltext2}</p>
                 </div>
               ))}
             </div>
             <div className="partner-page-cards">
-              {(subpages[0].cards || []).map((card) => (
+              {(subpageData[0].cards || []).map((card) => (
                 <div
                   key={card.id}
                   className={`partner-page-card ${card.type}`}
@@ -81,6 +93,56 @@ export default function PartnerPage() {
                 </div>
               ))}
             </div>
+            {subpageIdToLoad === 'podemos-ajudar' && formType !== '' ? (
+              <div className="partner-dynamic-content">
+                <h5>No que podemos melhorar</h5>
+                <div className="rounded-border-box">
+                  <navbar className="partner-page-navigation">
+                    <div>
+                      <NavLink to="#cidadao">Cidadão</NavLink>
+                    </div>
+                    <div>
+                      <NavLink to="#membro">Membro/Servidor</NavLink>
+                    </div>
+                  </navbar>
+                  <form className="partner-contact-form">
+                    <div className="inputs-box">
+                      <label htmlFor="nome">
+                        Nome
+                        <input type="text" id="nome" name="nome" />
+                      </label>
+                      <label htmlFor="email">
+                        E-mail
+                        <input type="text" id="email" name="email" />
+                      </label>
+                    </div>
+                    <div className="inputs-box">
+                      <label htmlFor="dataNascimento">
+                        Data de nascimento
+                        <input type="text" id="dataNascimento" name="dataNascimento" />
+                      </label>
+                      <label htmlFor="sexo">
+                        Sexo
+                        <input type="text" id="sexo" name="sexo" />
+                      </label>
+                      <label htmlFor="profissao">
+                        Profissão / Ocupação
+                        <input type="text" id="profissao" name="profissao" />
+                      </label>
+                    </div>
+                    <div className="inputs-box">
+                      <textarea placeholder="No que podemos melhorar?" />
+                    </div>
+                  </form>
+                </div>
+                <div className="partner-contact-form-action">
+                  <button type="button" className="partner-contact-button">
+                    enviar
+                    {formType}
+                  </button>
+                </div>
+              </div>
+            ) : null}
           </div>
         </div>
       </section>
