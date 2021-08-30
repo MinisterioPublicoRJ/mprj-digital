22573f2690521b055fe8825052f9007a2a16fbc1
renataandrade1987@hotmail.com <ministeriopublico@mac-camilla.local>
2021-08-30 16:49:10 -0300

    Ajuste hover transition

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,13 @@
-/* eslint-disable */
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { scroller } from 'react-scroll';
 import { BgHeader, NavHeader, ButtonHeader } from './index';
-import OPENDATA from '../openData/MockOpenData';
 import { MOCKPRODUTOSHEADER } from './mockProdutosHeader';
 import { MOCKBUTTONHEADER } from './mockButtonHeader';
+
 import {
   header,
   sectionProducts,
@@ -13,25 +17,11 @@ import {
   sectionExploreTexts,
   sectionBoxButton,
   sectionBoxLinks,
+  sectionBoxLine,
 } from './Header.module.css';
 
 export default function Header() {
   const [changeData, setchangeData] = useState('MPRJDigital');
-  const [productTitle, setProductTitle] = useState('');
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const filteredRepositories = OPENDATA.filter((repositories) =>
-        repositories.title
-          .toLowerCase()
-          .replace(/[\u0300-\u036f]/g, '')
-          .includes(productTitle.toLowerCase()),
-      );
-
-    };
-    fetchData();
-  }, [productTitle]);
 
   return (
     <header className={header}>
@@ -51,60 +41,106 @@ export default function Header() {
             />
           ))}
         </div>
-        
-        <NavHeader {...MOCKPRODUTOSHEADER.find((btn) => btn.id === changeData)}/>
+
+        <NavHeader {...MOCKPRODUTOSHEADER.find((btn) => btn.id === changeData)} />
         <div className={sectionProductsInput}>
           <i className="fa fa-search" aria-hidden="true" />
           <input
             type="text"
-            value={productTitle}
-            onChange={(event) => setProductTitle(event.target.value)}
-
+            value=""
+            onChange=""
           />
-         <p>Buscar base dados</p>
+          <p>Buscar base dados</p>
         </div>
       </section>
       <section className={sectionExplore}>
-        <div className={sectionExploreTexts}>
-          <h3>O que é o MPRJ Digital?</h3>
-          <p>
-            Iniciativa de transparência efetiva do MPRJ para demonstrar os resultados de sua
-            atividade baseada no modelo de governos digitais.
-          </p>
-        <div className={sectionBoxButton}>
-            <button>
-            <a className={sectionBoxLinks} href="/produto/parquet_digital">Saiba mais</a>
-            </button>
+        <div>
+          <div className={sectionExploreTexts}>
+            <h3>O que é o MPRJ Digital?</h3>
+            <p>
+              Iniciativa de transparência efetiva do MPRJ para demonstrar os resultados de sua
+              atividade baseada no modelo de governos digitais.
+            </p>
+            <div className={sectionBoxButton}>
+              <Link
+                className={sectionBoxLinks}
+                to="/"
+                onClick={() => scroller.scrollTo('mprjDigital', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 400,
+                })
+              }
+              >
+                <p>Saiba mais</p>
+              </Link>
+            </div>
           </div>
+          <div className={sectionBoxLine} />
         </div>
-        <div className={sectionExploreTexts}>
-          <h3>Parceiros e Produtos</h3>
-          <p>
-            Aqui você encontrará diversos produtos oriundos da política de governos digitais
-            empreendida pelo MPRJ.
-          </p>
-          <div className={sectionBoxButton}>
-            <button>
-              <a className={sectionBoxLinks} href="/produto/parquet_digital">Parceiros</a>
-            </button>
-            <button>
-            <a className={sectionBoxLinks} href="/produto/parquet_digital">Produtos</a>
-            </button>
+        <div>
+          <div className={sectionExploreTexts}>
+            <h3>Parceiros e Produtos</h3>
+            <p>
+              Aqui você encontrará diversos produtos oriundos da política de governos digitais
+              empreendida pelo MPRJ.
+            </p>
+            <div className={sectionBoxButton}>
+              <Link
+                className={sectionBoxLinks}
+                to="/"
+                onClick={() => scroller.scrollTo('parceiros', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 400,
+                })
+              }
+              >
+                <p>Parceiros</p>
+              </Link>
+              <Link
+                type="button"
+                style={{ marginLeft: 30 }}
+                className={sectionBoxLinks}
+                to="/"
+                onClick={() => scroller.scrollTo('produtos', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 400,
+                })
+                }
+              >
+                <p>Produtos</p>
+              </Link>
+            </div>
           </div>
+          <div className={sectionBoxLine} />
         </div>
-        <div className={sectionExploreTexts}>
-          <h3>Repositório de Dados</h3>
-          <p>
-            Confira a integridade de nossas soluções desde os dados e conheça, explore e baixe tudo
-            o que for interessante para realizar suas pesquisas, estudos e soluções.
-          </p>
-          <div className={sectionBoxButton}>
-            <button>
-            <a className={sectionBoxLinks} href="/produto/parquet_digital">Acessar</a>
-            </button>
+        <div>
+          <div className={sectionExploreTexts}>
+            <h3>Repositório de Dados</h3>
+            <p>
+              Confira a integridade de nossas soluções desde os dados explore e baixe tudo
+              o que for interessante para realizar suas pesquisas.
+            </p>
+            <div className={sectionBoxButton}>
+              <Link
+                className={sectionBoxLinks}
+                to="/"
+                onClick={() => scroller.scrollTo('repositorios', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 400,
+                })
+                }
+              >
+                <p>Acessar</p>
+              </Link>
+            </div>
           </div>
+          <div className={sectionBoxLine} />
         </div>
-        </section>
+      </section>
     </header>
   );
 }
