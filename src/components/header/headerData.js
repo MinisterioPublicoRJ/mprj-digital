99430b8512503f634c65fbfbb1ed66b44d88148a
majard <mahjard@protonmail.com>
2021-10-04 17:42:14 -0300

    Adds new products text to home menu

--- src/components/header/headerData.js
+++ src/components/header/headerData.js
@@ -24,6 +24,46 @@ const HEADER_DATA = [
     subtitle:
       'Fomentar a gestão comparada a partir da construção de indicadores e buscar por maior capacidade de resposta do MPRJ. O Farol permite olhar o mesmo parâmetro em diferentes unidades do MPRJ e sinalizar o que pode ser aprimorado.',
   },
+  {
+    id: 'DiagnósticoDasPromotorias',
+    backgroundImage: FAROLBG,
+    title: 'Diagnóstico das Promotorias',
+    link: '/produto/farol',
+    subtitle:
+      'Como o anuário estatístico é historicamente a forma mais comum de análise do trabalho das promotorias, a medida de vistas abertas em procedimentos por promotoria é muito valorizada dentro do MPRJ. Tendo isso em conta, faltava uma ferramenta que agregasse esses dados entregando visualizações em tempo real e permitindo o comparativo, sem que fossem ocultados os procedimentos de outra atribuição, que também geram trabalho (ainda que mínimo) em uma promotoria. O Diagnóstico é a ferramenta que ataca estes problemas.',
+  },
+  {
+    id: 'PainelDeCompras',
+    backgroundImage: FAROLBG,
+    title: 'Painel de Compras',
+    link: '/produto/farol',
+    subtitle:
+      'Fiscalizar todas as compras realizadas pelo Estado é um grande desafio, mas um que deve ser enfrentado, considerando a necessidade de boa gestão do dinheiro público e a importância do fator sobrepreço em compras públicas como condição que permite a corrupção. Para auxiliar a atividade fiscalizadora do MPRJ sobre a corrupção e a má gestão no Estado do Rio de Janeiro, montamos um painel que traz todos os dados de compras públicas realizadas pelos órgãos de projeção estadual do Estado do Rio de Janeiro e trazemos alertas que apontam quando uma contratação foi 30% mais cara que a mais barata nos últimos 6 meses, em respeito ao critério de sobrepreço adotado pelos tribunais superiores (STF e STJ).',
+  },
+  {
+    id: 'Lyra',
+    backgroundImage: FAROLBG,
+    title: 'Lyra',
+    link: '/produto/farol',
+    subtitle:
+      'Diante da necessidade de descobrir se algumas empresas cometiam a mesma infração contra vários consumidores, criamos o robô com capacidade de identificar a reiteração de demandas similares contra os mesmos réus. O intuito inicial era apurar quais infrações contra os consumidores não eram somente infrações pontuais, pessoais, mas sim infrações em larga escala, de caráter coletivo, que poderiam ser tratadas pelas Promotorias de Justiça do Consumidor no MPRJ. Conforme o sucesso da ferramenta foi verificado, seu uso foi estendido para encontrar demandas repetitivas que podem ser trabalhadas de forma especial pela segunda instância do Judiciário.',
+  },
+  {
+    id: 'GestaoDoTerritorio',
+    backgroundImage: FAROLBG,
+    title: 'Gestão do Território',
+    link: '/produto/farol',
+    subtitle:
+      'Para que seja possível efetivar políticas de desenvolvimento sustentável na capital fluminense, é necessário haver formulação de estratégias para atuação integrada dos órgãos de execução da defesa da ordem urbanística. O MPRJ pode auxiliar nesta tarefa, inclusive com intervenção judicial em favor da população, mas precisava de diversas informações que permitissem diagnósticos sobre a situação do território antes da intervenção. A ferramenta Gestão do Território se destina a resolver este problema, com foco em celeridade e eficiência na atuação.',
+  },
+  {
+    id: 'PainelSaneamento',
+    backgroundImage: FAROLBG,
+    title: 'Painel Saneamento',
+    link: '/produto/farol',
+    subtitle:
+      'As crises da geosmina e da falta d’água em meio à pandemia do novo coronavírus demonstraram a importância do saneamento básico para a população do Estado do Rio de Janeiro, especialmente os serviços de água, esgoto e drenagem. Diante disso e das possibilidades do MPRJ de fiscalização na matéria, foi elaborado um projeto de coleta de dados junto às Prefeituras e em bases públicas para permitir o diagnóstico da situação do Saneamento Básico em cada localidade do Estado. O intuito é que, para o público em geral, os cidadãos possam conhecer quais são os dados vistos pelo MPRJ na sua atividade fiscalizatória e apontar quais informações declaradas pelas prefeituras estão incorretas, permitindo a verificação. Já para o MPRJ, o objetivo é que a soma das informações disponibilizadas e os questionamentos dos cidadãos apoiem a atuação de membros e peritos do MPRJ.',
+  },
 ];
 
 export default HEADER_DATA;
