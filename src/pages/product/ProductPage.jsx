eff4a3eaf0c6fb99adadbd9aea8080085d6bc18a
renataandrade1987@hotmail.com <ministeriopublico@mac-camilla.local>
2021-10-08 18:24:20 -0300

    Cleaning files no used

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -20,50 +20,51 @@ export default function Produto() {
 
   useEffect(() => loadProductData(), [productName]);
 
-  return (
-    productData
-      ? (
-        <article className="productPage-outer">
-          <div className="productPage-img" style={{ backgroundImage: `url(${productData.bannerUrl})` }} />
-          <div className="productPage-presentation">
-            <img src={productData.icon} alt={`Ícone ${productData.title}`} />
-            <h2>{productData.subtitle}</h2>
-            <p>{productData.description}</p>
-          </div>
-          <h1 className="productPage-productName">{productData.title}</h1>
-          <div className="productPage-owner">
-            <span>{productData.organ}</span>
-            <span>Orgão Responsável</span>
-          </div>
-          <div className="productPage-service">
-            <span>{productData.service}</span>
-            <span>Serviço</span>
-          </div>
-          <div className="productPage-tabNavigation">
-            {productData.subsectionsArray.map(({ subsectionTitle, subsectionDescription }) => (
-              <button
-                key={subsectionTitle}
-                onClick={() => {
-                  setchangeData(subsectionTitle);
-                }}
-                className={`productPage-navButtons ${subsectionTitle === changeData ? 'productPage-navButtons-active' : ''
-                }`}
-                type="button"
-              >
-                {subsectionTitle}
-              </button>
-            ))}
-          </div>
-          <DataProduct
-            {...productData.subsectionsArray.find(
-              (subsection) => subsection.subsectionTitle === changeData,
-            )
-            }
-            textBtn={`Acessar ${productData.title}`}
-            url={productData.url}
-          />
-        </article>
-      )
-      : <h1>Carregando</h1>
+  return productData ? (
+    <article className="productPage-outer">
+      <div
+        className="productPage-img"
+        style={{ backgroundImage: `url(${productData.bannerUrl})` }}
+      />
+      <div className="productPage-presentation">
+        <img src={productData.icon} alt={`Ícone ${productData.title}`} />
+        <h2>{productData.subtitle}</h2>
+        <p>{productData.description}</p>
+      </div>
+      <h1 className="productPage-productName">{productData.title}</h1>
+      <div className="productPage-owner">
+        <span>{productData.organ}</span>
+        <span>Orgão Responsável</span>
+      </div>
+      <div className="productPage-service">
+        <span>{productData.service}</span>
+        <span>Serviço</span>
+      </div>
+      <div className="productPage-tabNavigation">
+        {productData.subsectionsArray.map(({ subsectionTitle, subsectionDescription }) => (
+          <button
+            key={subsectionTitle}
+            onClick={() => {
+              setchangeData(subsectionTitle);
+            }}
+            className={`productPage-navButtons ${
+              subsectionTitle === changeData ? 'productPage-navButtons-active' : ''
+            }`}
+            type="button"
+          >
+            {subsectionTitle}
+          </button>
+        ))}
+      </div>
+      <DataProduct
+        {...productData.subsectionsArray.find(
+          (subsection) => subsection.subsectionTitle === changeData,
+        )}
+        textBtn={`Acessar ${productData.title}`}
+        url={productData.url}
+      />
+    </article>
+  ) : (
+    <h1>Carregando</h1>
   );
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -9,20 +9,15 @@ function Router() {
   const location = useLocation();
 
   return (
-    /* <TransitionGroup>
+    <TransitionGroup>
       <CSSTransition key={location.key} classNames="fade" timeout={400}>
         <Routes location={location}>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/produto/:key" element={<ProductPage />} />
+          <Route exact path="/produto/:productName" element={<ProductPage />} />
           <Route path="/parceiro/:partnerId/:subpageId" element={<PartnerPage />} />
         </Routes>
       </CSSTransition>
-    </TransitionGroup> */
-    <Routes location={location}>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/produto/:productName" element={<ProductPage />} />
-      <Route path="/parceiro/:partnerId/:subpageId" element={<PartnerPage />} />
-    </Routes>
+    </TransitionGroup>
   );
 }
 
