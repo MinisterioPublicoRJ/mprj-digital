4c1d0e63b158962e583a1d1962ada1ccee31465a
majard <mahjard@protonmail.com>
2021-10-13 19:09:54 -0300

    Adds new text and images to header

--- src/components/header/headerData.js
+++ src/components/header/headerData.js
@@ -1,4 +1,4 @@
-import { FAROLBG, MPBG, PARQUET } from '../../assets/ImgsHeader';
+import { FAROLBG, MPBG, PARQUET, DIAGNOSTICO, PAINELDECOMPRAS, LYRA, GESTAODOTERRITORIO, PAINELSANEAMENTO } from '../../assets/ImgsHeader';
 
 const HEADER_DATA = [
   {
@@ -26,43 +26,43 @@ const HEADER_DATA = [
   },
   {
     id: 'DiagnósticoDasPromotorias',
-    backgroundImage: FAROLBG,
+    backgroundImage: DIAGNOSTICO,
     title: 'Diagnóstico das Promotorias',
     link: '/produto/farol',
     subtitle:
-      'Como o anuário estatístico é historicamente a forma mais comum de análise do trabalho das promotorias, a medida de vistas abertas em procedimentos por promotoria é muito valorizada dentro do MPRJ. Tendo isso em conta, faltava uma ferramenta que agregasse esses dados entregando visualizações em tempo real e permitindo o comparativo, sem que fossem ocultados os procedimentos de outra atribuição, que também geram trabalho (ainda que mínimo) em uma promotoria. O Diagnóstico é a ferramenta que ataca estes problemas.',
+      'Ferramenta que agrega dados de vistas abertas em procedimentos por promotoria, entregando visualizações em tempo real e permitindo o comparativo entre órgãos.',
   },
   {
     id: 'PainelDeCompras',
-    backgroundImage: FAROLBG,
+    backgroundImage: PAINELDECOMPRAS,
     title: 'Painel de Compras',
     link: '/produto/farol',
     subtitle:
-      'Fiscalizar todas as compras realizadas pelo Estado é um grande desafio, mas um que deve ser enfrentado, considerando a necessidade de boa gestão do dinheiro público e a importância do fator sobrepreço em compras públicas como condição que permite a corrupção. Para auxiliar a atividade fiscalizadora do MPRJ sobre a corrupção e a má gestão no Estado do Rio de Janeiro, montamos um painel que traz todos os dados de compras públicas realizadas pelos órgãos de projeção estadual do Estado do Rio de Janeiro e trazemos alertas que apontam quando uma contratação foi 30% mais cara que a mais barata nos últimos 6 meses, em respeito ao critério de sobrepreço adotado pelos tribunais superiores (STF e STJ).',
+      'Painel que se destina a apresentar todos os dados de compras públicas realizadas pelos órgãos de projeção estadual do Estado do Rio de Janeiro.',
   },
   {
     id: 'Lyra',
-    backgroundImage: FAROLBG,
+    backgroundImage: LYRA,
     title: 'Lyra',
     link: '/produto/farol',
     subtitle:
-      'Diante da necessidade de descobrir se algumas empresas cometiam a mesma infração contra vários consumidores, criamos o robô com capacidade de identificar a reiteração de demandas similares contra os mesmos réus. O intuito inicial era apurar quais infrações contra os consumidores não eram somente infrações pontuais, pessoais, mas sim infrações em larga escala, de caráter coletivo, que poderiam ser tratadas pelas Promotorias de Justiça do Consumidor no MPRJ. Conforme o sucesso da ferramenta foi verificado, seu uso foi estendido para encontrar demandas repetitivas que podem ser trabalhadas de forma especial pela segunda instância do Judiciário.',
+      'Um robô com a missão de auxiliar o cidadão fluminense e os Promotores de Justiça, capaz de ler todos os documentos do Tribunal de Justiça do Estado do Rio de Janeiro em velocidade inigualável e apontar quais demandas são repetitivas ou de caráter coletivo no Judiciário',
   },
   {
     id: 'GestaoDoTerritorio',
-    backgroundImage: FAROLBG,
+    backgroundImage: GESTAODOTERRITORIO,
     title: 'Gestão do Território',
     link: '/produto/farol',
     subtitle:
-      'Para que seja possível efetivar políticas de desenvolvimento sustentável na capital fluminense, é necessário haver formulação de estratégias para atuação integrada dos órgãos de execução da defesa da ordem urbanística. O MPRJ pode auxiliar nesta tarefa, inclusive com intervenção judicial em favor da população, mas precisava de diversas informações que permitissem diagnósticos sobre a situação do território antes da intervenção. A ferramenta Gestão do Território se destina a resolver este problema, com foco em celeridade e eficiência na atuação.',
+      'Ferramenta de mapas que permite o acesso a dados indispensáveis para o diagnóstico das ocorrências de ocupação irregular na cidade do Rio de Janeiro.',
   },
   {
     id: 'PainelSaneamento',
-    backgroundImage: FAROLBG,
+    backgroundImage: PAINELSANEAMENTO,
     title: 'Painel Saneamento',
     link: '/produto/farol',
     subtitle:
-      'As crises da geosmina e da falta d’água em meio à pandemia do novo coronavírus demonstraram a importância do saneamento básico para a população do Estado do Rio de Janeiro, especialmente os serviços de água, esgoto e drenagem. Diante disso e das possibilidades do MPRJ de fiscalização na matéria, foi elaborado um projeto de coleta de dados junto às Prefeituras e em bases públicas para permitir o diagnóstico da situação do Saneamento Básico em cada localidade do Estado. O intuito é que, para o público em geral, os cidadãos possam conhecer quais são os dados vistos pelo MPRJ na sua atividade fiscalizatória e apontar quais informações declaradas pelas prefeituras estão incorretas, permitindo a verificação. Já para o MPRJ, o objetivo é que a soma das informações disponibilizadas e os questionamentos dos cidadãos apoiem a atuação de membros e peritos do MPRJ.',
+      'Ferramenta para a visualização de dados referentes a água, esgoto, drenagem e o orçamento público gasto com essas matérias nos 92 munícios do Estado. Traz contextualização territorial, comparativos e permite a elaboração de relatórios.',
   },
 ];
 
