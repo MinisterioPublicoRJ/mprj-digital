03446a50f416bd6c31f818211e56baac64dad22a
Dione Martins <profissionaldione@gmail.com>
2021-11-17 16:35:30 -0300

    Reviewed Footer CSS

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,40 +4,52 @@ import { NavLink } from 'react-router-dom';
 import { scroller } from 'react-scroll';
 import logoMp from '../../assets/logoMp.svg';
 import logoOuvidoria from '../../assets/Ouvidoria.png';
-import styles from './Footer.module.css';
+import {
+  footerOuter,
+  footerMenu,
+  footerMenuColumn,
+  footerLogo,
+  footerCreateSpace,
+  footerMenuIcons,
+  footerMenuIconTwitter,
+  footerMenuIconInstagram,
+  footerMenuIconFacebook,
+  footerIconOuvidoria,
+  buttonBackToTop,
+} from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <div className={styles.footerOuter} id="rodape">
-      <div className={styles.footerMenu}>
-        <div className={styles.footerMenuColumn}>
-          <img className={styles.logoFooter} src={logoMp} alt="logo" />
-          <span className={styles.createSpace}>© Ministério Público do Estado do Rio de Janeiro</span>
-          <div className={styles.footerMenuIcons}>
+    <div className={footerOuter} id="rodape">
+      <div className={footerMenu}>
+        <div className={footerMenuColumn}>
+          <img className={footerLogo} src={logoMp} alt="logo" />
+          <span className={footerCreateSpace}>© Ministério Público do Estado do Rio de Janeiro</span>
+          <div className={footerMenuIcons}>
             <a
               href="https://twitter.com/MP_RJ"
               target="new"
               rel="noreferrer"
-              className={styles.footerMenuIconTwitter}
+              className={footerMenuIconTwitter}
               aria-label="Twitter"
             />
             <a
               href="https://www.instagram.com/mprj.oficial/"
               target="new"
               rel="noreferrer"
-              className={styles.footerMenuIconInstagram}
+              className={footerMenuIconInstagram}
               aria-label="Instagram"
             />
             <a
               href="https://www.facebook.com/MPRJ.Oficial/"
               target="new"
               rel="noreferrer"
-              className={styles.footerMenuIconFacebook}
+              className={footerMenuIconFacebook}
               aria-label="Facebook"
             />
           </div>
         </div>
-        <div className={styles.footerMenuColumn}>
+        <div className={footerMenuColumn}>
           <h3>PRODUTOS EM DESTAQUE</h3>
           <a href="/produto/parquet-digital" rel="noreferrer">
             Parquet Digital
@@ -46,12 +58,12 @@ export default function Footer() {
             Farol
           </a>
         </div>
-        <div className={styles.footerMenuColumn}>
+        <div className={footerMenuColumn}>
           <h3>MPRJ DIGITAL</h3>
           <a href="#mprjDigital">Quem Somos?</a>
           <a href="#parceiros">Parceiros</a>
         </div>
-        <div className={styles.footerMenuColumn}>
+        <div className={footerMenuColumn}>
           <h3>OUVIDORIA</h3>
           <a
             href="https://www.mprj.mp.br/comunicacao/ouvidoria/formulario"
@@ -67,11 +79,11 @@ export default function Footer() {
             rel="noreferrer"
             className="create-space"
           >
-            <img className={styles.iconOuvidoria} src={logoOuvidoria} alt="Logo Ouvidoria MPRJ" />
+            <img className={footerIconOuvidoria} src={logoOuvidoria} alt="Logo Ouvidoria MPRJ" />
           </a>
         </div>
       </div>
-      <button type="button" className={styles.buttonBackToTop}>
+      <button type="button" className={buttonBackToTop}>
         <NavLink
           to="/"
           type="button"
