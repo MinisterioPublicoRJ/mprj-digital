6d65d91bb6ca041b239d8b65d79bd184786af160
Layla <laylacsa@msn.com>
2021-11-17 17:55:55 -0300

    Adds reactGA and initializes settings

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
 import './App.css';
 
 import { BrowserRouter } from 'react-router-dom';
@@ -6,12 +8,16 @@ import Router from '../router/Router';
 import { Footer, Navbar } from '../components';
 import '../themes/themes.css';
 
-const App = () => (
-  <BrowserRouter>
-    <Navbar />
-    <Router />
-    <Footer />
-  </BrowserRouter>
-);
+const App = () => {
+  const trackingCode = process.env.ANALYTICS_CODE;
+  ReactGA.initialize(trackingCode);
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Router />
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
