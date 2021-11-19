7f8f927c81885c27e1a685fcec8487030beae514
Layla <laylacsa@msn.com>
2021-11-19 16:12:05 -0300

    Adding back linter

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { scroller } from 'react-scroll';
@@ -43,104 +41,94 @@ export default function Navbar() {
   useEffect(() => loadProductNavbarData(), []);
 
   return (
-        <div id="allnavBar">
-          {mobile && (
-            <button
-              className={`${navbarMobileButton}  ${mobileMenu && `${navbarMobileButtonActive}`}`}
-              aria-label="Menu"
-              type="button"
-              onClick={() => setMobileMenu(!mobileMenu)}
-            />
-          )}
-          <section
-            id="navBar"
-            className={`${mobile ? `${navbarMobile}` : `${navbarClass}`} ${
-              mobileMenu && `${navbarMobileButtonActive}`
-            }`}
-          >
-            <NavLink to="/">
-              <img className={navbarLogo} src={logoMp} alt="logo-Mp" />
+    <div id="allnavBar">
+      {mobile && (
+        <button
+          className={`${navbarMobileButton}  ${mobileMenu && `${navbarMobileButtonActive}`}`}
+          aria-label="Menu"
+          type="button"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        />
+      )}
+      <section
+        id="navBar"
+        className={`${mobile ? `${navbarMobile}` : `${navbarClass}`} ${
+          mobileMenu && `${navbarMobileButtonActive}`
+        }`}
+      >
+        <NavLink to="/">
+          <img className={navbarLogo} src={logoMp} alt="logo-Mp" />
+        </NavLink>
+        <section className={navbarLinks}>
+          <div className={navbarSectionToolTip}>
+            <NavLink
+              to="/"
+              onClick={() => scroller.scrollTo('mprjDigital', {
+                smooth: true,
+                offset: -70,
+                duration: 600,
+              })}
+            >
+              O que é o MPRJ Digital?
             </NavLink>
-            <section className={navbarLinks}>
-              <div className={navbarSectionToolTip}>
-                <NavLink
-                  to="/"
-                  onClick={() =>
-                    scroller.scrollTo('mprjDigital', {
-                      smooth: true,
-                      offset: -70,
-                      duration: 600,
-                    })
-                  }
-                >
-                  O que é o MPRJ Digital?
-                </NavLink>
-                <span className={navbarTooltipText}>Conheça a nossa visão de dados abertos</span>
+            <span className={navbarTooltipText}>Conheça a nossa visão de dados abertos</span>
+          </div>
+          <div className={navbarSectionToolTip}>
+            <NavLink to="/parceiro/gadg/sobre">Equipe</NavLink>
+          </div>
+          <div className={navbarDropdown}>
+            <div className={navbarSectionToolTip}>
+              <NavLink to="/" type="button" className={navbarDropbtnButton} id={navbarBasicButton}>
+                Produtos
+              </NavLink>
+              <span className={navbarTooltipText} style={{ marginBottom: 8 }}>
+                Navegue e conheça nossos produtos e sistemas
+              </span>
+              <div className={navbarDropdownContent}>
+                {productNavbarData
+                  ? productNavbarData.map(({ title, name }) => (
+                    <NavLink
+                      key={title}
+                      to={`/produto/${name}`}
+                      type="button"
+                      className={navbarDropbtn}
+                    >
+                      {title}
+                    </NavLink>
+                  ))
+                  : null}
               </div>
-              <div className={navbarSectionToolTip}>
-                <NavLink
-                  to={`/parceiro/gadg/sobre`}
-                >
-                  Equipe
-                </NavLink>
-              </div>
-              <div className={navbarDropdown}>
-                <div className={navbarSectionToolTip}>
-                  <NavLink to="/" type="button" className={navbarDropbtnButton} id={navbarBasicButton}>
-                    Produtos
-                  </NavLink>
-                  <span className={navbarTooltipText} style={{ marginBottom: 8 }}>
-                    Navegue e conheça nossos produtos e sistemas
-                  </span>
-                  <div className={navbarDropdownContent}>
-                    {productNavbarData
-                      ? productNavbarData.map(({ title, name }) => (
-                          <NavLink
-                            key={title}
-                            to={`/produto/${name}`}
-                            type="button"
-                            className={navbarDropbtn}
-                          >
-                            {title}
-                          </NavLink>
-                        ))
-                      : null}
-                  </div>
-                </div>
-              </div>
-              <div className={navbarSectionToolTip}>
-                <NavLink
-                  to="/"
-                  onClick={() =>
-                    scroller.scrollTo('repositorios', {
-                      smooth: true,
-                      offset: -70,
-                      duration: 600,
-                    })
-                  }
-                >
-                  Repositórios
-                </NavLink>
-                <span className={navbarTooltipText}>Encontre aqui todas as nossas Acervo de Dados</span>
-              </div>
-              <div className={navbarSectionToolTip}>
-                <NavLink
-                  to="/"
-                  type="button"
-                  onClick={() =>
-                    scroller.scrollTo('rodape', {
-                      smooth: true,
-                      offset: -70,
-                      duration: 600,
-                    })
-                  }
-                >
-                  Denúncias e Reclamações
-                </NavLink>
-                <span className={navbarTooltipText}>Tem uma Reclamação ou precisa da nossa ajuda.</span>
-              </div>
-            </section>
-          </section>
-        </div>
+            </div>
+          </div>
+          <div className={navbarSectionToolTip}>
+            <NavLink
+              to="/"
+              onClick={() => scroller.scrollTo('repositorios', {
+                smooth: true,
+                offset: -70,
+                duration: 600,
+              })}
+            >
+              Repositórios
+            </NavLink>
+            <span className={navbarTooltipText}>Encontre aqui todas as nossas Acervo de Dados</span>
+          </div>
+          <div className={navbarSectionToolTip}>
+            <NavLink
+              to="/"
+              type="button"
+              onClick={() => scroller.scrollTo('rodape', {
+                smooth: true,
+                offset: -70,
+                duration: 600,
+              })}
+            >
+              Denúncias e Reclamações
+            </NavLink>
+            <span className={navbarTooltipText}>Tem uma Reclamação ou precisa da nossa ajuda.</span>
+          </div>
+        </section>
+      </section>
+    </div>
   );
 }
