38fb2ef6ce99188897898e7d06f65a6104908286
Dione Martins <profissionaldione@gmail.com>
2021-11-25 19:21:45 -0300

    Finished multi column dropdown

--- src/components/navbar/dropdownProducts/DropdownProducts.jsx
+++ src/components/navbar/dropdownProducts/DropdownProducts.jsx
@@ -3,24 +3,54 @@ import { NavLink } from 'react-router-dom';
 import PropTypes, { object } from 'prop-types';
 
 import {
-  navbarDropbtn,
+  dropdownWrapper,
+  dropdownHighlights,
+  dropdownOtherProducts,
+  dropdownButtons,
+  dropdownButtonsWrapper,
 } from './DropdownProducts.module.css';
 
 export default function DropdownProducts({ productsData }) {
+  const highlitedCount = 2;
+  const highlitedProductsData = productsData ? productsData.slice(0, highlitedCount) : null;
+  const otherProductsData = productsData ? productsData.slice(highlitedCount) : null;
+
   return (
-    <div>
-      {productsData
-        ? productsData.map(({ title, name }) => (
-          <NavLink
-            key={title}
-            to={`/produto/${name}`}
-            type="button"
-            className={navbarDropbtn}
-          >
-            {title}
-          </NavLink>
-        ))
-        : null}
+    <div className={dropdownWrapper}>
+      <div className={dropdownHighlights}>
+        <h2>Produtos em destaque</h2>
+        <div className={dropdownButtonsWrapper}>
+          {highlitedProductsData
+            ? highlitedProductsData.map(({ title, name }) => (
+              <NavLink
+                key={title}
+                to={`/produto/${name}`}
+                type="button"
+                className={dropdownButtons}
+              >
+                {title}
+              </NavLink>
+            ))
+            : null}
+        </div>
+      </div>
+      <div className={dropdownOtherProducts}>
+        <h2>Outros produtos</h2>
+        <div className={dropdownButtonsWrapper}>
+          {otherProductsData
+            ? otherProductsData.map(({ title, name }) => (
+              <NavLink
+                key={title}
+                to={`/produto/${name}`}
+                type="button"
+                className={dropdownButtons}
+              >
+                {title}
+              </NavLink>
+            ))
+            : null}
+        </div>
+      </div>
     </div>
   );
   /*
