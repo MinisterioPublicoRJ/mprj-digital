e45a3470209f9eb49f04d18225ec157be591e58e
Dione Martins <profissionaldione@gmail.com>
2021-11-25 23:15:16 -0300

    First version of the multi column products done

--- src/components/navbar/dropdownProducts/DropdownProducts.jsx
+++ src/components/navbar/dropdownProducts/DropdownProducts.jsx
@@ -3,21 +3,34 @@ import { NavLink } from 'react-router-dom';
 import PropTypes, { object } from 'prop-types';
 
 import {
-  dropdownWrapper,
-  dropdownHighlights,
-  dropdownOtherProducts,
+  dropdownOuter,
+  dropdownInner,
   dropdownButtons,
   dropdownButtonsWrapper,
+  dropdownColumns,
 } from './DropdownProducts.module.css';
+
+function otherProductsData(remainingProductsData) {
+  const tempArray = [remainingProductsData];
+  const mediumColumnSize = 4;
+  const columnCount = Math.round(remainingProductsData.length / mediumColumnSize);
+  const maxPerColumn = Math.ceil(remainingProductsData.length / columnCount);
+
+  for (let i = 0; i < columnCount; i += 1) {
+    const start = i * maxPerColumn;
+    tempArray[i] = remainingProductsData.slice(start, start + maxPerColumn);
+  }
+
+  return tempArray;
+}
 
 export default function DropdownProducts({ productsData }) {
   const highlitedCount = 2;
   const highlitedProductsData = productsData ? productsData.slice(0, highlitedCount) : null;
-  const otherProductsData = productsData ? productsData.slice(highlitedCount) : null;
 
   return (
-    <div className={dropdownWrapper}>
-      <div className={dropdownHighlights}>
+    <div className={dropdownOuter}>
+      <div className={dropdownInner}>
         <h2>Produtos em destaque</h2>
         <div className={dropdownButtonsWrapper}>
           {highlitedProductsData
@@ -34,39 +47,34 @@ export default function DropdownProducts({ productsData }) {
             : null}
         </div>
       </div>
-      <div className={dropdownOtherProducts}>
+      <div className={dropdownInner}>
         <h2>Outros produtos</h2>
-        <div className={dropdownButtonsWrapper}>
-          {otherProductsData
-            ? otherProductsData.map(({ title, name }) => (
-              <NavLink
-                key={title}
-                to={`/produto/${name}`}
-                type="button"
-                className={dropdownButtons}
+        <div className={dropdownColumns}>
+          {productsData
+            ? otherProductsData(productsData.slice(highlitedCount)).map((item) => (
+              <div
+                className={dropdownButtonsWrapper}
+                key={`column${item[0].name}`}
               >
-                {title}
-              </NavLink>
+                {productsData
+                  ? item.map(({ title, name }) => (
+                    <NavLink
+                      key={title}
+                      to={`/produto/${name}`}
+                      type="button"
+                      className={dropdownButtons}
+                    >
+                      {title}
+                    </NavLink>
+                  ))
+                  : null}
+              </div>
             ))
             : null}
         </div>
       </div>
     </div>
   );
-  /*
-  {productsData
-    ? productsData.map(({ title, name }) => (
-      <NavLink
-        key={title}
-        to={`/produto/${name}`}
-        type="button"
-        className={navbarDropbtn}
-      >
-        {title}
-      </NavLink>
-    ))
-    : null}
-  */
 }
 
 DropdownProducts.propTypes = {
