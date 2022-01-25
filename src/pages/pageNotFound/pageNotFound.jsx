270d28faf63f96b4047e0bfcb9c366927b4a1a5f
re-andrade1987 <renataandrade1987@hotmail.com>
2022-01-25 13:43:14 -0300

    Creating basic structure to page not found

---
+++ src/pages/pageNotFound/pageNotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './PageNotFound.modules.css';
+
+export default function PageNotFound (){
+    return(
+        <h1>Paágina não encontrada</h1>
+    )
+}
