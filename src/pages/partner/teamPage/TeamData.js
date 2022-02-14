cd90f89112ecd0b8ed34c548f2eccc319d20c323
re-andrade1987 <renataandrade1987@hotmail.com>
2022-02-14 16:03:16 -0300

    Creating fake json to get data

---
+++ src/pages/partner/teamPage/TeamData.js
@@ -0,0 +1,40 @@
+// import CSI from '../../assets/logos/csi.png';
+
+export const TEAM_CONST = [
+  {
+    id: 'csi',
+    employeeName: 'Renata Andrade',
+    // img: CSI,
+  },
+  {
+    id: 'gate',
+    name: 'GATE',
+    // img: GATE,
+  },
+  {
+    id: 'inova',
+    name: 'INOVA',
+    // img: INOVA,
+  },
+  {
+    id: 'sgmp',
+    name: 'SGMP',
+    // img: SGMP,
+  },
+  {
+    id: 'stic',
+    name: 'STIC',
+    // img: STIC,
+  },
+  {
+    id: 'subplan',
+    name: 'SUBPLAN',
+    // img: SUBPLAN,
+  },
+  {
+    id: 'iep',
+    name: 'IEP',
+    // img: IEP,
+  },
+];
+export { TEAM_CONST as default };
