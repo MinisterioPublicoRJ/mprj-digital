e487420f4d74fa7c08882aea24a500843597c0f4
Marcele Monteiro <marcelepmonteiro@gmail.com>
2022-02-16 14:04:56 -0300

    Update grid layout in partner page cards

--- src/pages/partner/productsPartnerItem/ProductPartnerItem.jsx
+++ src/pages/partner/productsPartnerItem/ProductPartnerItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './ProductPartnerItem.css';
 import miniaturaDefault from '../../../assets/produto-miniatura-default.png';
